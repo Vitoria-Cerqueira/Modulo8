608f3f20d97ad91e86f9a15ce315cd0960c70622
Vitoria.Cerqueira <vitoria.cerqueira@zup.com.br>
2023-07-19 16:26:29 -0300

    Subindo exercicios

--- src/main/kotlin/Ex2/Main.kt
+++ src/main/kotlin/Ex2/Main.kt
@@ -1,7 +1,53 @@
 package Ex2
 
-fun main(){
-    val verdura = Verdura("",0.0)
-    verdura.cadastrarProduto()
+import kotlin.system.exitProcess
 
+private val verdura = Verdura("",0.0)
+private val graos = Graos("",0.0)
+private val legume = Legumes("",0)
+private val outro = Outros("",0)
+
+fun main(){
+    menu()
+}
+
+fun menu(){
+    try {
+        println("\nVocê deseja comprar qual tipo de alimento:\n1-Verdura | 2-Legumes | 3-Grãos | 4-Outros | 5-Sair")
+        val opcao= readln().toInt()
+        when(opcao){
+            1 -> { verdura.cadastrarProduto()}
+            2 -> { legume.cadastrarProduto()}
+            3 -> { graos.cadastrarProduto()}
+            4 -> { outro.cadastrarProduto()}
+            5 -> {
+                println("Obrigado(a) por comprar conosco!")
+                System.exit(0) }
+            else -> {throw IllegalArgumentException("Tipo de alimento inválido")}
+        }
+    }catch (exception: java.lang.IllegalArgumentException){
+        exception.printStackTrace()
+        println("Causa: ${exception.cause}")
+        println("Mensagem: ${exception.message}")
+        main()
+    }
+    finally {
+        println("\nDeseja continuar comprando?\n1-Sim | 2-Não")
+        when(readln().toInt()){
+            1 -> {
+                menu()
+                println() }
+            2 -> { verdura.mostrarValores()
+                println()
+                legume.mostrarValores()
+                println()
+                graos.mostrarValores()
+                println()
+                outro.mostrarValores()
+                exitProcess(0)
+            }
+            else -> { println("Opção inválida!")
+                exitProcess(0) }
+        }
+    }
 }
--- src/main/kotlin/Ex2/Produtos.kt
+++ src/main/kotlin/Ex2/Produtos.kt
@@ -2,32 +2,36 @@ package Ex2
 
 abstract class ProdutosGramas(var nome: String, var pesoEmGrama: Double){
     abstract fun cadastrarProduto()
-    abstract fun mostrarValores(arrayList: ArrayList<ProdutosGramas>)
+    abstract fun mostrarValores()
 
 }
 abstract class ProdutosPeso(var nome: String, var pesoEmUnidades: Int){
     abstract fun cadastrarProduto()
-    abstract fun mostrarValores(arrayList: ArrayList<ProdutosPeso>)
+    abstract fun mostrarValores()
 }
 
 class Verdura(nome: String, pesoEmGrama: Double): ProdutosGramas(nome, pesoEmGrama){
     var listaVerdura = arrayListOf<Verdura>()
 
     override fun cadastrarProduto() {
-        var continuar = true
-        do {
+
+
             try {
                 println("Informe o nome do produto:")
                 var nome = readln()
+                while (nome.isNullOrBlank()){
+                    println("O nome não pode ser em branco, digite novamente: ")
+                    nome = readln()
+                }
+
                 println("Informe a quantidade em gramas que deseja comprar:")
                 var grama = readln()
                 if (grama.isNullOrBlank()) {
-                    continuar = false
                     throw UnsupportedOperationException()
                 }
 
-                if (!grama.contains('.') || grama.contains('-')){
-                    continuar = false
+                if (!grama.contains('.') || grama.contains('-')|| grama == "0"){
+
                     throw NumberFormatException()
                 }
 
@@ -44,34 +48,40 @@ class Verdura(nome: String, pesoEmGrama: Double): ProdutosGramas(nome, pesoEmGra
                 println("Mensagem: Para verdura, a quantidade deve ser informada com ponto")
 
             }
-        }while (!continuar)
+
 
     }
 
 
-    override fun mostrarValores(arrayList: ArrayList<ProdutosGramas>) {
+    override fun mostrarValores() {
         for (i in listaVerdura){
             println("Nome: ${i.nome}\nPeso em gramas: ${i.pesoEmGrama}")
         }
+        println("A quantidade de verduras da lista de compras é ${listaVerdura.size}")
+
     }
 
 }
 class Graos(nome: String, pesoEmGrama: Double): ProdutosGramas(nome, pesoEmGrama){
     var listaGraos = arrayListOf<Graos>()
     override fun cadastrarProduto() {
-        var continuar = true
-        do {
+
+
             try {
                 println("Informe o nome do produto:")
                 var nome = readln()
+                while (nome.isNullOrBlank()){
+                    println("O nome não pode ser em branco, digite novamente: ")
+                    nome = readln()
+                }
                 println("Informe a quantidade em gramas que deseja comprar:")
                 var grama = readln()
                 if (grama.isNullOrBlank()) {
                     throw UnsupportedOperationException()
                 }
 
-                if (!grama.contains('.') || grama.contains('-')){
-                    continuar = false
+                if (!grama.contains('.') || grama.contains('-')|| grama == "0"){
+
                     throw NumberFormatException()
                 }
 
@@ -85,36 +95,114 @@ class Graos(nome: String, pesoEmGrama: Double): ProdutosGramas(nome, pesoEmGrama
             }catch (exception:NumberFormatException){
                 exception.printStackTrace()
                 println("Causa: ${exception.cause}")
-                println("Mensagem: Para verdura, a quantidade deve ser informada com ponto")
+                println("Mensagem: Para Graos, a quantidade deve ser informada com ponto")
 
             }
-        }while (!continuar)
+
     }
 
-    override fun mostrarValores(arrayList: ArrayList<ProdutosGramas>) {
+    override fun mostrarValores() {
         for (i in listaGraos){
             println("Nome: ${i.nome}\nPeso em gramas: ${i.pesoEmGrama}")
         }
+        println("A quantidade de Graos da lista de compras é ${listaGraos.size}")
+
     }
 
 }
 class Legumes(nome: String, pesoEmUnidades: Int): ProdutosPeso(nome, pesoEmUnidades){
+    var listaLegumes = arrayListOf<Legumes>()
     override fun cadastrarProduto() {
+
+
+            try {
+                println("Informe o nome do produto:")
+                var nome = readln()
+                while (nome.isNullOrBlank()){
+                    println("O nome não pode ser em branco, digite novamente: ")
+                    nome = readln()
+                }
+                println("Informe a quantidade em gramas que deseja comprar:")
+                var grama = readln()
+                if (grama.isNullOrBlank()) {
+                    throw UnsupportedOperationException()
+                }
+
+                if (grama.contains('.') || grama.contains(",") ||grama.contains('-')|| grama == "0"){
+
+                    throw NumberFormatException()
+                }
+
+                listaLegumes.add(Legumes(nome, grama.toInt()))
+
+            }catch (exception : UnsupportedOperationException){
+                exception.printStackTrace()
+                println("Causa: ${exception.cause}")
+                println("Não é permitido inserir valor vazio.")
+
+            }catch (exception:NumberFormatException){
+                exception.printStackTrace()
+                println("Causa: ${exception.cause}")
+                println("Mensagem: Para Legumes, a quantidade deve ser informado em inteiro")
+
+            }
+
 
     }
 
-    override fun mostrarValores(arrayList: ArrayList<ProdutosPeso>) {
-
+    override fun mostrarValores() {
+        for (i in listaLegumes){
+            println("Nome: ${i.nome}\nPeso em gramas: ${i.pesoEmUnidades}")
+        }
+        println("A quantidade de legumes da lista de compras é ${listaLegumes.size}")
     }
 
 }
 class Outros(nome: String, pesoEmUnidades: Int): ProdutosPeso(nome, pesoEmUnidades){
+    var listaOutros = arrayListOf<Outros>()
     override fun cadastrarProduto() {
+
+
+            try {
+                println("Informe o nome do produto:")
+                var nome = readln()
+                while (nome.isNullOrBlank()){
+                    println("O nome não pode ser em branco, digite novamente: ")
+                    nome = readln()
+                }
+
+                println("Informe a quantidade em gramas que deseja comprar:")
+                var grama = readln()
+                if (grama.isNullOrBlank()) {
+                    throw UnsupportedOperationException()
+                }
+
+                if (grama.contains('.') || grama.contains(",") ||grama.contains('-')|| grama == "0"){
+
+                    throw NumberFormatException()
+                }
+
+                listaOutros.add(Outros(nome, grama.toInt()))
+
+            }catch (exception : UnsupportedOperationException){
+                exception.printStackTrace()
+                println("Causa: ${exception.cause}")
+                println("Não é permitido inserir valor vazio.")
+
+            }catch (exception:NumberFormatException){
+                exception.printStackTrace()
+                println("Causa: ${exception.cause}")
+                println("Mensagem: Para Outros, a quantidade deve ser informada inteira")
+
+            }
 
     }
 
-    override fun mostrarValores(arrayList: ArrayList<ProdutosPeso>) {
-
+    override fun mostrarValores() {
+        for (i in listaOutros){
+            println("Nome: ${i.nome}\nPeso em gramas: ${i.pesoEmUnidades}")
+        }
+        println("A quantidade de outros da lista de compras é ${listaOutros.size}")
     }
 
 }
